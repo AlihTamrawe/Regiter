f26991a4856987e8a16144225e4fa1a78927a0dd
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2024-01-08 23:27:12 +0200

    colors and transform

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { StyleSheet,Button, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 export default function LoginScreen() {
   return (
-    <View style={{flex:1, justifyContent:'center',backgroundColor:'red'}}>
-
+    <SafeAreaView style={{flex:1, justifyContent:'center',backgroundColor:'yellow', transform:[{rotate:'1deg'}]}}>
+    <View >
+      <Text style={{fontSize:50}}>Login Screen</Text>
     </View>
+    </SafeAreaView>
   )
 }
 
