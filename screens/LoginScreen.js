ff42db84ff3cb185ad97edcfc2853013945b12f7
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2024-01-10 23:29:00 +0200

    ui of login bage is ready

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,23 +7,28 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function LoginScreen() {
   return (
-    <SafeAreaView style={{flex:1,width:'100%', justifyContent:'center',backgroundColor:'yellow',margin:5 ,padding:50, transform:[{rotate:'1deg'}]}}>
+    <SafeAreaView style={{flex:1,width:'100%', justifyContent:'center',backgroundColor:'#0C8BA50',margin:5 ,padding:50, transform:[{rotate:'1deg'}]}}>
       <View>
       <Text style={{fontSize:50}}>Login Screen</Text>
       </View>
-      <View  style={{ flexDirection:'row',alignItems:'center',borderBottomColor:'#ccc',paddingHorizontal:25, marginRight:5,marginBottom:5, padding:15,width:400, flexDirection:'row', alignContent:'space-around',backgroundColor:'red' ,width:'auto', transform:[{rotate:'-1deg'}]}} >
+      <View  style={{ flexDirection:'row',alignItems:'center',borderBottomColor:'#ccc',paddingHorizontal:25, marginRight:5,marginBottom:5, padding:15,width:400, flexDirection:'row', alignContent:'space-around',backgroundColor:'white' ,width:'auto', transform:[{rotate:'-1deg'}]}} >
           <MaterialIcons name='alternate-email' size={20} color='#066'/>
           <TextInput placeholder='Email ID'style={{ fontWeight:'500',height:20,width:'auto'}}/>
       </View>
-      <View style={{ color:'#666',flexDirection:'row', marginRight:5,marginBottom:5,borderBottomWidth:1, justifyContent:'center', padding:15,width:400, flexDirection:'row', alignContent:'space-around',backgroundColor:'red' ,width:'auto', transform:[{rotate:'-1deg'}]}} >
+      <View style={{ color:'#666',flexDirection:'row', marginRight:5,marginBottom:5,borderBottomWidth:1, justifyContent:'center', padding:15,width:400, flexDirection:'row', alignContent:'space-around',backgroundColor:'white' ,width:'auto', transform:[{rotate:'-1deg'}]}} >
       <Ionicons name='ios-lock-closed-outline' size={20} color='#166'/>
       <TextInput
         placeholder='password ID' 
         style={{flex:1,paddingVertical:0}}
         secureTextEntry={true}
        />
-       <TouchableOpacity onPress={()=>{}}>
-        <Text >forget</Text>
+       <TouchableOpacity onPress={()=>{}} >
+        <Text style={{fontWeight:'700',fontSize:16,color:'#AD40AF'}} >forget</Text>
+       </TouchableOpacity>
+      </View>
+      <View>
+      <TouchableOpacity  onPress={()=>{}} style={{alignItems:'center',backgroundColor:'#AD40AF',padding:20,borderRadius:10,marginBottom:30}}>
+        <Text style={{fontWeight:'700',fontSize:16,color:'#fff'}} >Login</Text>
        </TouchableOpacity>
       </View>
     
