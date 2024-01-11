433ed68eb023e4787682d856603ec0279f4d0ebe
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2024-01-12 00:42:26 +0200

    successful svg file show it inside the login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,8 +4,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import GoogleSvg from '../assets/google.svg'
-import FacebookSvg from '../assets/facebook.svg'
+
+import Logo from '../assets/google.svg';
+import Svg, { Path } from "react-native-svg"
 
 
 export default function LoginScreen() {
@@ -40,9 +41,13 @@ export default function LoginScreen() {
       </Text>
     
       <TouchableOpacity style={{borderColor:'#ddd',borderWidth:2,borderRadius:10,paddingHorizontal:30,paddingVertical:10}}>
-         <GoogleSvg height={24} width={24}></GoogleSvg> 
-         <FacebookSvg  height={24} width={24}></FacebookSvg>
-      </TouchableOpacity>
+      
+          
+    
+      <Svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><Path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"/>
+      <Path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"/>
+      </Svg>
+            </TouchableOpacity>
     </SafeAreaView>
   )
 }
