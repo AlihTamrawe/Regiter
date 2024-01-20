55d244abb7924db40dce199c7f18ac56f1a37afb
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2024-01-21 01:00:37 +0200

    COMMIT

--- component/CustomButton.js
+++ component/CustomButton.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { StyleSheet,Button, Text, View,TextInput, TouchableOpacity,Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
+// import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Svg, { Path } from "react-native-svg"
 export default function CustomButton({lable,onPress}) {
